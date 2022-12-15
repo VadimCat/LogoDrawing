0f672aa4ab84691f7ed086526d1dcc765b48b4df
Aleksey_Gladkov <a.gladkov@severex.io>
2022-12-16 00:48:21 +0300

    backgrounds were loaded one by one from the array, just in a circle

---
+++ Assets/Scripts/Utils/CircularLinkedList.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Utils
+{
+    static class CircularLinkedList {
+        public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> current)
+        {
+            return current.Next ?? current.List.First;
+        }
+
+        public static LinkedListNode<T> PreviousOrLast<T>(this LinkedListNode<T> current)
+        {
+            return current.Previous ?? current.List.Last;
+        }
+    }
+}
