7812f3533bc78954fab347f3dc8b10728783db96
Aleksey_Gladkov <a.gladkov@severex.io>
2022-12-22 20:26:39 +0300

    add background saving

--- Assets/Scripts/Client/UI/Screens/BackgroundService.cs
+++ Assets/Scripts/Client/UI/Screens/BackgroundService.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Utils;
-using Random = UnityEngine.Random;
 
 namespace Client.Screens
 {
@@ -15,13 +13,14 @@ namespace Client.Screens
         [SerializeField] private Sprite loadingBack;
 
         [SerializeField] private Sprite[] levelBackgroundImages;
-        private readonly LinkedList<Sprite> backgrounds = new();
-        private LinkedListNode<Sprite> currentBackground;
+
+        private BackgroundsList backgrounds;
+        private Sprite currentBackground;
+
 
         private void Awake()
         {
-            FillLinkedList();
-            currentBackground = backgrounds.Last;
+            backgrounds = new BackgroundsList(levelBackgroundImages);
             SceneManager.activeSceneChanged += HandleSceneChanged;
         }
 
@@ -34,14 +33,6 @@ namespace Client.Screens
             transform.position = pos;
         }
 
-        private void FillLinkedList()
-        {
-            foreach (var t in levelBackgroundImages)
-            {
-                backgrounds.AddLast(t);
-            }
-        }
-
         public void SwitchBackground(Background background)
         {
             switch (background)
@@ -50,8 +41,8 @@ namespace Client.Screens
                     backRoot.sprite = loadingBack;
                     break;
                 case Background.Game:
-                    currentBackground = currentBackground.NextOrFirst();
-                    backRoot.sprite = currentBackground.Value;
+                    currentBackground = backgrounds.GetNext();
+                    backRoot.sprite = currentBackground;
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(background), background, null);
---
+++ Assets/Scripts/Utils/BackgroundsList.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public class BackgroundsList
+    {
+        private const string BACKGROUND_INDEX_SAVE_KEY = "background_index";
+        private readonly Sprite[] backgrounds;
+        private int index;
+
+        public BackgroundsList(Sprite[] backgrounds)
+        {
+            this.backgrounds = backgrounds;
+            index = LoadLastIndex();
+            Debug.Log($"index constructor= {index}");
+        }
+
+        public Sprite GetNext()
+        {
+            Debug.Log($"index = {index}");
+            index++;
+            index = index == backgrounds.Length ? 0 : index;
+            SaveLastIndex();
+            return backgrounds[index];
+        }
+
+        private void SaveLastIndex() => PlayerPrefs.SetInt(BACKGROUND_INDEX_SAVE_KEY, index);
+        private int LoadLastIndex() => PlayerPrefs.GetInt(BACKGROUND_INDEX_SAVE_KEY) - 1;
+    }
+}
